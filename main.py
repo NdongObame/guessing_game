6c953e6a947403bf6ab72a1a6459ed5314bc2a2a
NdongObame <Jeremiejoshua@gmail.com>
2023-01-12 22:46:28 +0200

    guessing game 1srt stage (update01:better code to avoid any error for invalid input at the ebd of the game. next update will firstly focus on doing so but for the begining of the game)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+###stage1###
+
 print("welcome player, the rules are simple.\nYou have a word to guess, 5 hints and 5 tries")
 ready = input("are you ready ? \n types \"y\" or \"n\":")
 if ready == "n":
@@ -6,7 +8,8 @@ elif ready == "y":
     print("i like your enthusiasm")
 else:
     print("if you cant answer that, i see no hope for the word; here it is")
-answer = "Mclaren"
+answer = "mclaren"
+answer_check = "Mclaren"
 hints = {1: "i am a word of 7 letters",
          2: "Mcdonals's biggest inspiration",
          3: "if you haven't guessed yet that i am a car you're really bad at this game",
@@ -14,33 +17,34 @@ hints = {1: "i am a word of 7 letters",
          5: "Maybe that's why i am a sporty",
          6: "i mainly do super cars",
          7: "my name rhymes with karen but i'm definitely not one"}
-print(hints[1])
-guess_1 = input("what's the word ?:")
 
 
-def boucle(number_of_tries, x, player_guess):
-    while number_of_tries != 0 and player_guess != answer:
+def boucle(number_of_tries,x):
+    print(hints[1])
+    guess_1 = input("what's the word ?:").lower()
+    while number_of_tries != 0 and guess_1 != answer :
         print("nope genuis," + str(number_of_tries) + " chances left")
-        print(hints[x + 1])
         x += 1
         number_of_tries -= 1
-        player_guess = input("what's the word ?:")
-    return player_guess
+        print(hints[x])
+        guess_1 = input("what's the word ?:").lower()
+    return guess_1
 
-
-if boucle(4, 1, guess_1) == answer:
+if boucle(4,1) == answer:
     print("correct, smarter than i thought")
 else:
     print("well no surprise genius, you lost")
     retry = input("more hints or should i give you the answer ?\n type 1 or 2:")
+    while retry != "1" and retry != "2":
+            print("invalid answer, 1 or 2 genuis")
+            retry = input("more hints or should i give you the answer ?\n type 1 or 2:")
     if retry == "1":
         print("alright two more chances to guess!")
         print(hints[6])
-        player_guess = input("what's the word ?:")
-        if boucle(1, 6, guess_1) == answer:
+        guess_1 = input("what's the word ?:").lower()
+        if boucle(1, 6) == answer:
             print("correct, finally, i guess it's better late than never")
         else:
-            print("no hope for you. The answer is " + answer)
-
+            print("no hope for you. The answer is " + answer_check)
     elif retry == "2":
-        print("well loser, the answer was " + answer)
+        print("well loser, the answer was " + answer_check)
